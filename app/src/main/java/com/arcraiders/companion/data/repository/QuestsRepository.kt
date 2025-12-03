91210371643facae31735968cbd9078031d8711f
ministerofsalt-cell <ministerofsalt@gmail.com>
2025-12-03 10:06:41 +0000

    Create QuestsRepository.kt

---
+++ app/src/main/java/com/arcraiders/companion/data/repository/QuestsRepository.kt
@@ -0,0 +1,29 @@
+package com.arcraiders.companion.data.repository
+
+import com.arcraiders.companion.data.api.MetaforgeApi
+import com.arcraiders.companion.data.dao.QuestDao
+import com.arcraiders.companion.data.model.Quest
+import com.arcraiders.companion.data.model.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class QuestsRepository @Inject constructor(
+    private val api: MetaforgeApi,
+    private val dao: QuestDao
+) {
+    fun getQuests(): Flow<List<Quest>> = dao.getAllQuests().map { entities ->
+        entities.map { it.toQuest() }
+    }
+
+    suspend fun refreshQuests() {
+        try {
+            val quests = api.getQuests()
+            dao.insertAll(quests.map { it.toEntity() })
+        } catch (e: Exception) {
+            // Handle error
+        }
+    }
+}
